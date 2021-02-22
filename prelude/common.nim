016f3544b9de2db0b9d13e9b65c107cdf70a755f
name <name@example.com>
2021-02-22 18:16:42 -0500

    added macro and compileTime funcs for getting obj field info

--- prelude/common.nim
+++ prelude/common.nim
@@ -15,33 +15,5 @@ import pkg/[error, debug, log]
 import ./alias, ./override
 export alias, error, debug, log, override
 
-
-func getFieldInfoNodes*[T: object](self: typedesc[T]; fieldIdent: string): (NimNode, NimNode) {.compileTime.} =
-  for field in getTypeImpl(self)[2]:
-    if field[0].strVal == fieldIdent:
-      return (field[1], newLit(field[0].isExported))
-
-func getFieldInfoNodes*(self: NimNode; fieldIdent: string): (NimNode, NimNode) {.compileTime.} =
-  getImpl(self).matchAst:
-  of nnkTypeDef(_, _, nnkObjectTy(_, _, `params` @ nnkRecList)):
-    for field in params:
-      var ident = field[0].repr
-      if ident[^1] == '*':
-        if ident[0..^2] == fieldIdent:
-          return (field[1], newLit true)
-      else:
-        if ident == fieldIdent:
-          return (field[1], newLit false)
-
-type ObjectFieldInfo*[T] = object
-  t: typedesc[T]
-  x: bool
-
-macro getFieldInfo*[T: object](self: typedesc[T]; field: untyped): ObjectFieldInfo =
-  macro inner(s, f): (NimNode, NimNode) =
-    quote do: getFieldInfoNodes(`s`, `f`.strVal)
-  let (t, x) = inner[T](self, field)
-  quote do: ObjectFieldInfo[`t`](x: `x`)
-
 #when compileOption("rangechecks"):
 #when compileOption("rangechecks"): 
--- prelude/override.nim
+++ prelude/override.nim
@@ -3,54 +3,33 @@
 #   - This module probably needs a better name.
 import pkg/prelude/[alias, lib]
 
-#macro override(a: typed{nkSym}; b: typed{nkPar|nkTupleConstr}): untyped =
-macro override(a, b: untyped): untyped =
-  result = newStmtList()
-  matchAst b:
-  of nnkPar:
-    for override in b:
-      alias ident, override[0]
-      alias val, override[1]
-      result.add quote do:
-        `a`.`ident` = `val`
-  of nnkIdent:
-    let i = b.repr
-    #result.add quote do:
-    #  `a`.override `b`
-  #echo result.repr
-#macro override(a, b: typed{nkSym}): untyped =
-#  result = newStmtList()
-#  echo b.getImpl().repr
-#  for override in b.getImpl():
-#    alias ident, override[0]
-#    alias val, override[1]
-#    result.add quote do:
-#      `a`.`ident` = `val`
-macro o(a: NimNode, b: typed{nkSym}) =
-  result = quote do:
-    `a`.override `b`
-macro oo[T: ref|object](a: T, b: typed{nkSym}): untyped =
-  result = quote do:
-    `a`.override `b`
-proc overrideNimNode(a, b: NimNode): NimNode {.compileTime.} =
-  expectKind b, nnkPar
-  result = newStmtList()
-  for override in b:
-    alias ident, override[0]
-    alias val, override[1]
-    result.add quote do:
-      `a`.`ident` = `val`
-proc overrideNimNode[T](a: var T, b: NimNode): NimNode {.compileTime.} =
-  expectKind b, nnkPar
-  result = newStmtList()
-  for override in b:
-    alias ident, override[0]
-    alias val, override[1]
-    a.a = 7
-    result.add quote do:
-      `a`.`override[0]` = `val`
-template t(a: untyped, b: untyped): untyped {.dirty.} =
-  a.override b
+func getFieldInfoNodes*[T: object](self: typedesc[T]; fieldIdent: string): (NimNode, NimNode) {.compileTime.} =
+  for field in getTypeImpl(self)[2]:
+    if field[0].strVal == fieldIdent:
+      return (field[1], newLit(field[0].isExported))
+
+func getFieldInfoNodes*(self: NimNode; fieldIdent: string): (NimNode, NimNode) {.compileTime.} =
+  getImpl(self).matchAst:
+  of nnkTypeDef(_, _, nnkObjectTy(_, _, `params` @ nnkRecList)):
+    for field in params:
+      var ident = field[0].repr
+      if ident[^1] == '*':
+        if ident[0..^2] == fieldIdent:
+          return (field[1], newLit true)
+      else:
+        if ident == fieldIdent:
+          return (field[1], newLit false)
+
+type ObjectFieldInfo*[T] = object
+  t*: typedesc[T]
+  x*: bool
+
+macro getFieldInfo*[T: object](self: typedesc[T]; field: untyped): ObjectFieldInfo =
+  macro inner(s, f): (NimNode, NimNode) =
+    quote do: getFieldInfoNodes(`s`, `f`.strVal)
+  let (t, x) = inner[T](self, field)
+  quote do: ObjectFieldInfo[`t`](x: `x`)
+
 
 when isMainModule and defined test:
   import std/unittest
@@ -60,27 +39,30 @@ when isMainModule and defined test:
 
     setup:
       type A = object
-        a, b, c: int
+        a, b, c*: int
 
-    test "tuple can be a subset of the object's fields":
-      var a = A(b: 1, c: 2)
-      a.override (a: 1, b: 2)
-      check a.a == 1
-      check a.b == 2
-      check a.c == 2
+    test "a":
+      check getFieldInfo(A, b).x == false
 
-    test "calling from a macro":
-      macro setA(a: untyped): untyped =
-        expectKind a, nnkPar
-        result = newStmtList()
-        var b: A
-        b.override(a)
-        assert b.a == 7
-        result = quote do:
-          var b = A()
-          b.override(`a`)
-          check b.a == 7
-      setA (a: 7)
+#    test "tuple can be a subset of the object's fields":
+#      var a = A(b: 1, c: 2)
+#      a.override (a: 1, b: 2)
+#      check a.a == 1
+#      check a.b == 2
+#      check a.c == 2
+#
+#    test "calling from a macro":
+#      macro setA(a: untyped): untyped =
+#        expectKind a, nnkPar
+#        result = newStmtList()
+#        var b: A
+#        b.override(a)
+#        assert b.a == 7
+#        result = quote do:
+#          var b = A()
+#          b.override(`a`)
+#          check b.a == 7
+#      setA (a: 7)
 
 #    test "fusion.astdsl composition":
 #      macro setB(a, b: untyped): untyped =
